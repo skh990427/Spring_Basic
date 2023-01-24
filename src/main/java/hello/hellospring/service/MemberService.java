55a861002fa0e7ac390f194f470257580a87cba8
KwangHo <skh990427@naver.com>
2023-01-24 21:32:32 +0900

    회원가입 서비스

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,31 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원 X 원래는 같은 이름이 있을수는 있지만 예시니깐 임의로 정함
+        validataDuplicateMember(member); //중복회원 검증
+
+        memberRepository.save(member); //중복회원 검증이 통과하면 검증
+        return member.getId();
+    }
+
+    private void validataDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+}
