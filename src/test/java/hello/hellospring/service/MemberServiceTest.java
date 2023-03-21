d15843adc85965bcd327b7f1d4571fe980a41600
KwangHo <skh990427@naver.com>
2023-03-21 17:41:39 +0900

    회원가입 서비스 테스트코드 작성

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -35,7 +35,6 @@ class MemberServiceTest {
         //when
         Long saveId = memberService.join(member);
 
-        //
         //then
         Member findMember = memberService.findOne(saveId).get();
         assertThat(member.getName()).isEqualTo(findMember.getName());
