b4bb88111d358b772c9336dfb3f7c57fa237828f
KwangHo <skh990427@naver.com>
2023-03-21 21:41:23 +0900

    자바 코드로 직접 스프링 빈 등록하기

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -8,8 +8,27 @@ import org.springframework.stereotype.Controller;
 public class MemberController {
     private final MemberService memberService;
 
+    /**
+     * 생성자 주입
+     */
     @Autowired //생성자에 @Autowired 라고 되어있으면 이 멤버 서비스를 스프링이 스프링빈에 있는 멤버 서비스를 가져다가 연결 시켜준다
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    /**
+     * setter 주입
+     */
+/*
+    private MemberService memberService;
+
+    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+*/
+
+    //@Autowired private MemberService memberService; 이거는 필드 주입이라고 한다. 근데 스프링에서는 별로 안좋아함
+
+
 }
